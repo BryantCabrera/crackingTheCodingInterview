acfa647063e69d3f21e41dc65e60b03b7d5b6e35
BryantCabrera <cabrera.bryant@gmail.com>
2020-07-10 21:02:17 -0700

    Creates solution for problem 1.1.

--- arraysAndStrings/isUnique.js
+++ arraysAndStrings/isUnique.js
@@ -1,1 +1,24 @@
 // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters.  What if you cannot use additional data structures?
+
+const isUnique = (str) => {
+	const hashMap = {};
+
+	let currentStrIndex = 0;
+	let strIsUnique = true;
+	while (currentStrIndex <= str.length) {
+		if (hashMap[str[currentStrIndex]] !== undefined) {
+			strIsUnique = false;
+			break;
+		} else {
+			hashMap[str.charAt(currentStrIndex)] = currentStrIndex;
+		}
+		currentStrIndex++;
+	}
+	return strIsUnique;
+};
+
+const sampleInput1 = 'aabcadb';
+const sampleInput2 = 'abcde';
+
+console.log(isUnique(sampleInput1));
+console.log(isUnique(sampleInput2));
