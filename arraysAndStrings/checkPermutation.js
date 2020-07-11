e373ddaec3dfc1afb0ac4985df55df9d6ca24abf
BryantCabrera <cabrera.bryant@gmail.com>
2020-07-10 21:32:25 -0700

    Creates solution for problem 1.2.

--- arraysAndStrings/checkPermutation.js
+++ arraysAndStrings/checkPermutation.js
@@ -1,2 +1,25 @@
 // 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
 
+const checkPermutation = (str1, str2) => {
+	const str1SortedArr = str1.split('').sort();
+	const str2SortedArr = str2.split('').sort();
+
+	let isPermutation = true;
+
+	// Traverse str1SortedArr linearly and check if the current character matches the character at the same index in str2SortedArr
+	for (let i = 0; i < str1SortedArr.length; i++) {
+		if (str1SortedArr[i] !== str2SortedArr[i]) {
+			isPermutation = false;
+			break;
+		}
+	}
+
+	return isPermutation;
+};
+
+const sampleInput1 = 'aabcadb';
+const sampleInput2 = 'abcde';
+const sampleInput3 = 'ecdba';
+
+console.log(checkPermutation(sampleInput1, sampleInput2));
+console.log(checkPermutation(sampleInput2, sampleInput3));
