2e09c6827c575ee0f4d4cc3eaf6427477ecdd325
BryantCabrera <cabrera.bryant@gmail.com>
2020-07-20 17:53:20 -0700

    Scaffolds solution to problem 1.9.

--- arraysAndStrings/stringRotation.js
+++ arraysAndStrings/stringRotation.js
@@ -1,1 +1,12 @@
-// 1.9 String Rotation: Assume you have a method isSubstring  which checks if one word is a substring of another.  Given two strings, s1 and s2, write code to check is s2 is a rotation of s1 using only one call to isSubstring (e.g. "waterbottle" is a rotation of "erbottlewat").
+// 1.9 String Rotation: Assume you have a method isSubstring which checks if one word is a substring of another.  Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g. "waterbottle" is a rotation of "erbottlewat").
+
+const isSubstring = (s1, s2) => {
+
+};
+
+const input1 = 'waterbottle';
+const input2 = 'erbottlewat';
+const input3 = 'erbotttlewat';
+
+console.log(isSubstring(input1, input2));
+console.log(isSubstring(input1, input3));
