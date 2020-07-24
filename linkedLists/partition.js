d7fb9e398aa5cecb6407f9ea56f59f316529909b
BryantCabrera <cabrera.bryant@gmail.com>
2020-07-23 23:00:59 -0700

    Adds problem 2.4.

---
+++ linkedLists/partition.js
@@ -0,0 +1,1 @@
+// 2.4 Partition: Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater than or equal to x.  If x is contained within the list, the values of x only need to be after the elements less than x.
