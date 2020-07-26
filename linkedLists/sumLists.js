e321df4ed528ac85102407fc296421da7ea42fbc
BryantCabrera <cabrera.bryant@gmail.com>
2020-07-26 10:42:24 -0700

    Adds capability to process inputs of different lengts in sumListsForward.

--- linkedLists/sumLists.js
+++ linkedLists/sumLists.js
@@ -32,13 +32,19 @@ const sumListsForward = (singlyLinkedList1, singlyLinkedList2) => {
 	let currentNode2 = singlyLinkedList2.head;
 	let num2 = '';
 
-	while (currentNode1 && currentNode2) {
-		// Store values as a string as we cannot tell how long the input linkedList is, and therefore cannot tell the current digit's place.
-		num1 += `${currentNode1.value}`;
-		num2 += `${currentNode2.value}`;
-
-		currentNode1 = currentNode1.next;
-		currentNode2 = currentNode2.next;
+	while (currentNode1 || currentNode2) {
+		if (currentNode1) {
+			// Store values as a string as we cannot tell how long the input linkedList is, and therefore cannot tell the current digit's place.
+			// Utilizes ternary to avoid concatenating any characters if the current node is null.  This will take care of inputs with different sizes/lengths.
+			num1 += `${currentNode1.value}`;
+			currentNode1 = currentNode1.next;
+		}
+		if (currentNode2) {
+			// Store values as a string as we cannot tell how long the input linkedList is, and therefore cannot tell the current digit's place.
+			// Utilizes ternary to avoid concatenating any characters if the current node is null.  This will take care of inputs with different sizes/lengths.
+			num2 += `${currentNode2.value}`;
+			currentNode2 = currentNode2.next;
+		}
 	}
 
 	// Convert strings into integers.
@@ -128,24 +134,64 @@ while (input2.size() < input2Values.length + 1) {
 
 const output1 = sumListsReverse(input1, input2); // Expect: 2 -> 1 -> 9.
 console.log(output1.size()); // Expect: 3.
-console.log(output1.head); // Expect: 2 -> 1 -> 9.
 
-const input3Values = [1, 7];
-const input3 = new SinglyLinkedList(new SinglyLinkedListNode(6));
+const input3Values = [1, 6];
+const input3 = new SinglyLinkedList(new SinglyLinkedListNode(7));
 let valuePointer3 = 0;
 while (input3.size() < input3Values.length + 1) {
 	input3.getLast().next = new SinglyLinkedListNode(input3Values[valuePointer3]);
 	valuePointer3++;
 }
 
-const input4Values = [9, 5];
-const input4 = new SinglyLinkedList(new SinglyLinkedListNode(2));
+const input4Values = [9, 2];
+const input4 = new SinglyLinkedList(new SinglyLinkedListNode(5));
 let valuePointer4 = 0;
 while (input4.size() < input4Values.length + 1) {
 	input4.getLast().next = new SinglyLinkedListNode(input4Values[valuePointer4]);
 	valuePointer4++;
 }
 
-const output2 = sumListsForward(input3, input4); // Expect: 9 -> 1 -> 2.
+const output2 = sumListsReverse(input1, input2); // Expect: 2 -> 1 -> 9.
 console.log(output2.size()); // Expect: 3.
-console.log(output2.head); // Expect: 9 -> 1 -> 2.
+console.log(output2.head); // Expect: 2 -> 1 -> 9.
+
+const input5Values = [1, 7];
+const input5 = new SinglyLinkedList(new SinglyLinkedListNode(6));
+let valuePointer5 = 0;
+while (input5.size() < input5Values.length + 1) {
+	input5.getLast().next = new SinglyLinkedListNode(input5Values[valuePointer5]);
+	valuePointer5++;
+}
+
+const input6Values = [9, 5];
+const input6 = new SinglyLinkedList(new SinglyLinkedListNode(2));
+let valuePointer6 = 0;
+while (input6.size() < input6Values.length + 1) {
+	input6.getLast().next = new SinglyLinkedListNode(input6Values[valuePointer6]);
+	valuePointer6++;
+}
+
+const output3 = sumListsForward(input5, input6); // Expect: 9 -> 1 -> 2.
+console.log(output3.size()); // Expect: 3.
+console.log(output3.head); // Expect: 9 -> 1 -> 2.
+
+const input7Values = [1, 7];
+const input7 = new SinglyLinkedList(new SinglyLinkedListNode(6));
+let valuePointer7 = 0;
+while (input7.size() < input7Values.length + 1) {
+	input7.getLast().next = new SinglyLinkedListNode(input7Values[valuePointer7]);
+	valuePointer7++;
+}
+
+const input8Values = [9, 5, 3];
+const input8 = new SinglyLinkedList(new SinglyLinkedListNode(2));
+let valuePointer8 = 0;
+while (input8.size() < input8Values.length + 1) {
+	input8.getLast().next = new SinglyLinkedListNode(input8Values[valuePointer8]);
+	valuePointer8++;
+}
+
+const output4 = sumListsForward(input7, input8); // Expect: 3 -> 5 -> 7 -> 0.
+console.log(output4.size()); // Expect: 4.
+console.log(output4.head); // Expect: 3 -> 5 -> 7 -> 0.
+console.log(output4.getLast().value); // Expect: 0.
