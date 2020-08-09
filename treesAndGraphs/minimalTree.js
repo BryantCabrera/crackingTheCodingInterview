d9149cac2dc84341177b3d2424cf767fa8e014aa
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-09 10:58:26 -0700

    Adds descriptive comments to 4.2 solution.

--- treesAndGraphs/minimalTree.js
+++ treesAndGraphs/minimalTree.js
@@ -1,12 +1,21 @@
 // 4.2 Minimal Tree: Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
 
 const minimalTree = (arr, startIndex, endIndex) => {
+	// EDGE CASE: If the array is null, return null.
+	if (arr.length === 0) return null;
+
+	// Once the endIndex is less than the startIndex, return null.
+	// This effectively begins processing a new branch.
 	if (endIndex < startIndex) return null;
 
+	// Find the value in the middle of the array.  This will be the root.
+	// We arbitrarily Math.floor because in the case of an array with an odd length, either the node to the left of the midpoint index value or to the right of the index value (e.g. 2 or 3 in the case the midpoint is 2.5) will suffice as the root.
 	const midpoint = Math.floor((startIndex + endIndex) / 2);
 
+	// Create a new BinaryTreeNode.
 	const node = new TreeNode(arr[midpoint]);
 
+	// Recursively create the left and right nodes of the current node.
 	node.left = minimalTree(arr, startIndex, midpoint - 1);
 	node.right = minimalTree(arr, midpoint + 1, endIndex);
 
---
+++ treesAndGraphs/utils/binaryTree.js
@@ -0,0 +1,36 @@
+// Implements a binary tree.
+class BinaryTree {
+	constructor (arr = []) {
+		this.root = this.createBinaryTree(arr, 0, arr.length - 1)
+	}
+
+	createBinaryTree (arr, startIndex, endIndex) {
+		// EDGE CASE: If the array is null, return null.
+		if (arr.length === 0) return null;
+
+		// Once the endIndex is less than the startIndex, return null.
+		// This effectively begins processing a new branch.
+		if (endIndex < startIndex) return null;
+		
+		// Find the value in the middle of the array.  This will be the root.
+		// We arbitrarily Math.floor because in the case of an array with an odd length, either the node to the left of the midpoint index value or to the right of the index value (e.g. 2 or 3 in the case the midpoint is 2.5) will suffice as the root.
+		const midpoint = Math.floor((startIndex + endIndex) / 2);
+		
+		// Create a new BinaryTreeNode.
+		const node = new BinaryTreeNode(arr[midpoint]);
+		
+		// Recursively create the left and right nodes of the current node.
+		node.left = minimalTree(arr, startIndex, midpoint - 1);
+		node.right = minimalTree(arr, midpoint + 1, endIndex);
+	
+		return node;
+	};
+}
+
+class BinaryTreeNode {
+	constructor (value) {
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+}
