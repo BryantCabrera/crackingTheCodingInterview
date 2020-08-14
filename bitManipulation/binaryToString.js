43f52f18d9fbcba1aa02ca16748764e801e95b40
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-13 21:17:33 -0700

    Adds problem 4.8.

--- bitManipulation/binaryToString.js
+++ bitManipulation/binaryToString.js
@@ -1,1 +1,8 @@
 // 5.2 Binary to String: Given a real number between 0 and 1 (e.g., 0.72) that is passed in as a double, print the binary representation.  If the number cannot be represented accurately in binary with at most 32 characters, print "ERROR."
+
+const binaryToString = (double) => {
+
+};
+
+const input1 = 72;
+console.log(binaryToString(input1)); // Expect: 0.10111000010100011111.
