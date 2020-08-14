5322e94bb643f5d520cb32b1625ab75a75963c4e
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-14 11:32:05 -0700

    Scaffolds solution to 5.6.

--- bitManipulation/conversion.js
+++ bitManipulation/conversion.js
@@ -1,1 +1,9 @@
 // 5.6 Conversion: Write a function to determine the number of bits you would need to flip to convert integer A to integer B.
+
+const conversion = (intA, intB) => {
+
+};
+
+const input1 = 0b11101; // 29 in base 10.
+const input2 = 0b01111; // 15 in base 10.
+console.log(conversion(input1, input2)); // Expect: 2.
