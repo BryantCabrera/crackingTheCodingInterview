af3603350977d58f91d68f16a0950d77e415c06c
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-14 11:17:17 -0700

    Adds problem 5.4.

--- bitManipulation/flipBitToWin.js
+++ bitManipulation/flipBitToWin.js
@@ -1,7 +1,10 @@
 // 5.3 Flip Bit to Win: You have an integer and you can flip exactly one bit from a 0 to a 1.  Write code to find the length of the longest sequence of 1s you could create.
 
-const flipBitToWin = () => {
+const flipBitToWin = (int) => {
+	const binaryInt = int.toString(2);
+	console.log(`binaryInt: ${binaryInt}`);
 
+	
 };
 
 const input1 = 1775; // Represented as 11011101111 in binary.
