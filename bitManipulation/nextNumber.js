3fb818a3d5214f52e83fc4b4943c2605eb8d3127
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-14 11:18:21 -0700

    Scaffolds solution to 5.4.

--- bitManipulation/nextNumber.js
+++ bitManipulation/nextNumber.js
@@ -1,1 +1,8 @@
 // 5.4 Next Number: Given a positive integer, print the next smallest and the next largest number that have the same number of 1 bits in their binary representation.
+
+const nextNumber = (int) => {
+
+};
+
+const input1 = 0b100101;
+console.log(nextNumber(input1));
