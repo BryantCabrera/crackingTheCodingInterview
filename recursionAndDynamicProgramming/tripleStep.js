4cb76ae58c4dc296cdc1ca9e45781bc7caded48d
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-18 10:57:02 -0700

    Scaffolds solution to problem 8.1.

--- recursionAndDynamicProgramming/tripleStep.js
+++ recursionAndDynamicProgramming/tripleStep.js
@@ -1,1 +1,7 @@
 // 8.1 Triple Step: A child is running up a staircase with n steps and can hop either 1 step, 2 steps, or 3 steps at a time.  Implement a method to count how many possible ways the child can run up the stairs.
+
+const tripleStep = (lengthOfStairs) => {
+
+};
+
+console.log(tripleStep(3)); // Expect: 4.
