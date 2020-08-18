fa964c4561f448ac3ba08b365216154e443bbd07
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-18 15:04:29 -0700

    Creates solution to 8.2.

--- recursionAndDynamicProgramming/robotInAGrid.js
+++ recursionAndDynamicProgramming/robotInAGrid.js
@@ -1,7 +1,30 @@
 // 8.2 Robot in a Grid: Imagine a robot sitting on the upper left corner of grid with r rows and c columns.  The robot can only move in two directions, right and down, but certain cells are "off limits" such that the robot cannot step on them.  Design an algorithm to find a path for the robot from the top left to the bottom right.
 
 const robotInAGrid = (grid) => {
+	const getPath = (entireGrid, row, col, path) => {
+		// If we are not withint the grid, this is not a valid path.
+		if (col < 0 || row < 0 || !entireGrid[row][col]) return false;
+
+		// If we are at the origin or there is a validPath from current cell to the cell in the next row OR the cell in the next column, this is a valid path.
+		if ((row === 0 &&  col === 0) || getPath(entireGrid, row - 1, col, path) || getPath(entireGrid, row, col - 1, path)) {
+			path.push([row, col]);
+			return true;
+		}
+
+		// If this isn't a valid path.
+		return false;
+	};
+
+	// If the grid is not valid, there is no valid path.
 	if (grid === null || grid.length === 0) return null;
+
+	const validPath = [];
+
+	if (getPath(grid, grid.length - 1, grid[0].length - 1, validPath)) {
+		return validPath;
+	}
+
+	return null;
 };
 
 const input1 = [
