7b78482079c66e210c33f7c7428856ccf7abb9a3
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-19 09:56:24 -0700

    Fixes test cases so that elements are sorted distinct ints.

--- recursionAndDynamicProgramming/magixIndex.js
+++ recursionAndDynamicProgramming/magixIndex.js
@@ -1,7 +1,11 @@
 // 8.3 Magic Index: A magic index in an array A[0...n - 1] is defined to be an index such that A[i] = i.  Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 
+// const magicIndex1 = (sortedArrWithDistinctInts) => {
+
+// };
+
 // Brute Force Solution
-const magicIndex1a = (sortedArrWithDistinctInts) => {
+const magicIndex1 = (sortedArrWithDistinctInts) => {
 	for (let i = 0; i <  sortedArrWithDistinctInts.length; i++) {
 		if (sortedArrWithDistinctInts[i] === i) {
 			return i;
@@ -11,11 +15,11 @@ const magicIndex1a = (sortedArrWithDistinctInts) => {
 	return null;
 };
 
-const input1 = [9, 8, 2, 0, 1, 3];
+const input1 = [-1, 0, 2, 5, 7, 9];
 console.log(magicIndex1(input1)); // Expect: 2.
 
-const input2 = [0, 9, 2, 8, 1, 7];
+const input2 = [0, 3, 4, 7, 8, 9];
 console.log(magicIndex1(input2)); // Expect: 0.
 
-const input3 = [9, 8, 7, 6, 5, 4];
+const input3 = [4, 5, 6, 7, 8, 9];
 console.log(magicIndex1(input3)); // Expect: null.
