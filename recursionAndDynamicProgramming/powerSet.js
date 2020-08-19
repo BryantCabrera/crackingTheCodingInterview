f118847cf3c6b889197e41b6c1a3ef81a30c9a90
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-19 16:42:06 -0700

    Scaffolds solution to 8.4.

--- recursionAndDynamicProgramming/powerSet.js
+++ recursionAndDynamicProgramming/powerSet.js
@@ -1,1 +1,27 @@
 // 8.4 Power Set: Write a method to return all subsets of a set.
+
+const powerSet = (set) => {
+	const subsets = [];
+
+	const createSet = (set, subsets) => {
+
+	};
+
+	return subsets;
+};
+
+const input1 = new Set();
+const input1values = [0, 1, 2];
+for (let value of input1values) {
+	input1.add(value);
+}
+console.log(powerSet(input1).length); // Expect: 6.
+console.log(powerSet(input1)); // Expect:
+// [
+// 	[0], 
+// 	[0, 1],
+// 	[0, 1, 2],
+// 	[1],
+// 	[1, 2],
+// 	[2]
+// ]
