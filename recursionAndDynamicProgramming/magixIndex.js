d27b21fa697c8902808659f43c6be3c421b55f93
BryantCabrera <cabrera.bryant@gmail.com>
2020-08-19 09:39:26 -0700

    Scaffolds solution to 8.3.

--- recursionAndDynamicProgramming/magixIndex.js
+++ recursionAndDynamicProgramming/magixIndex.js
@@ -1,1 +1,14 @@
 // 8.3 Magic Index: A magic index in an array A[0...n - 1] is defined to be an index such that A[i] = i.  Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
+
+const magicIndex1 = (sortedArrWithDistinctInts) => {
+
+};
+
+const input1 = [9, 9, 2, 9, 9, 9];
+console.log(magicIndex1(input1)); // Expect: 2.
+
+const input2 = [0, 9, 2, 9, 9, 9];
+console.log(magicIndex1(input2)); // Expect: 0, 2.
+
+const input3 = [9, 9, 9, 9, 9, 9];
+console.log(magicIndex1(input3)); // Expect: null.
