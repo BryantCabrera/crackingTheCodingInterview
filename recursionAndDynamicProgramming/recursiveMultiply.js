3cd40401400566d8ada143c25d430552d9d8725f
BryantCabrera <cabrera.bryant@gmail.com>
2020-10-31 16:20:01 -0700

    Adds test cases for 8.5.

--- recursionAndDynamicProgramming/recursiveMultiply.js
+++ recursionAndDynamicProgramming/recursiveMultiply.js
@@ -4,3 +4,9 @@ const recursiveMultiply = (num1, num2) => {
 
 };
 
+// Test Cases
+console.log(recursiveMultiply(1, 1)); // Expect: 1.
+console.log(recursiveMultiply(0, 1)); // Expect: 0.
+console.log(recursiveMultiply(0, 0)); // Expect: 0.
+console.log(recursiveMultiply(4, 5)); // Expect: 20.
+console.log(recursiveMultiply(20, 21)); // Expect: 420.
